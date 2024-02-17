91c36dbd081ce524731bfeb9b3c8c0b206d6f5d2
Mateo pizzolo <mateo@Mateos-Mac-mini.local>
2024-02-17 17:16:07 -0300

    Data Engineer

---
+++ DataEngineer/create_tables.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS data_2022_dec (
+    event_time TIMESTAMP,
+    event_type VARCHAR(255),
+    product_id INTEGER,
+    price NUMERIC(10, 2),
+    user_id INTEGER,
+    user_session UUID
+);
+
+CREATE TABLE IF NOT EXISTS data_2022_nov (
+    event_time TIMESTAMP,
+    event_type VARCHAR(255),
+    product_id INTEGER,
+    price NUMERIC(10, 2),
+    user_id INTEGER,
+    user_session UUID
+);
+
+CREATE TABLE IF NOT EXISTS data_2022_oct (
+    event_time TIMESTAMP,
+    event_type VARCHAR(255),
+    product_id INTEGER,
+    price NUMERIC(10, 2),
+    user_id INTEGER,
+    user_session UUID
+);
+
+CREATE TABLE IF NOT EXISTS data_2023_jan (
+    event_time TIMESTAMP,
+    event_type VARCHAR(255),
+    product_id INTEGER,
+    price NUMERIC(10, 2),
+    user_id INTEGER,
+    user_session UUID
+);
+
+
---
+++ DataEngineer/init.sql
@@ -0,0 +1,6 @@
+\i /docker-entrypoint-initdb.d/create_tables.sql
+
+\COPY data_2022_dec FROM '/docker-entrypoint-initdb.d/subject/customer/data_2022_dec.csv' DELIMITER ',' CSV HEADER;
+\COPY data_2022_nov FROM '/docker-entrypoint-initdb.d/subject/customer/data_2022_nov.csv' DELIMITER ',' CSV HEADER;
+\COPY data_2022_oct FROM '/docker-entrypoint-initdb.d/subject/customer/data_2022_oct.csv' DELIMITER ',' CSV HEADER;
+\COPY data_2023_jan FROM '/docker-entrypoint-initdb.d/subject/customer/data_2023_jan.csv' DELIMITER ',' CSV HEADER;
